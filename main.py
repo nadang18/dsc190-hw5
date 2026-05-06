c527d10a0010ec9e034a35ae7cfbea3847171e61
nadang18 <nadang@ucsd.edu>
2026-05-05 17:00:56 -0700

    fix lint errors

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy as NP
-
-def main(Text):
-    Text = "Hello from dsc190-hw5!"
-    print(Text)
+def main(text):
+    text = "Hello from dsc190-hw5!"
+    print(text)
  
 if __name__ == "__main__":
     main()
